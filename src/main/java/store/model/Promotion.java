cde5c985775a1184ce5893bec41cd69d571fc005
최영우 <allnstar@naver.com>
2024-11-08 17:52:14 +0900

    refactor: Dataarser에서 현재날짜에 해당하지 않는 프로모션 제외

--- src/main/java/store/model/Promotion.java
+++ src/main/java/store/model/Promotion.java
@@ -1,19 +1,17 @@
 package store.model;
 
-import java.util.Date;
-
 public class Promotion {
     private final String name;
     private final int buyCount;
     private final int getCount;
-    private final Date startDate;
-    private final Date endDate;
 
-    public Promotion(String name, int buyCount, int getCount, Date startDate, Date endDate) {
+    public Promotion(String name, int buyCount, int getCount) {
         this.name = name;
         this.buyCount = buyCount;
         this.getCount = getCount;
-        this.startDate = startDate;
-        this.endDate = endDate;
+    }
+
+    public String getName() {
+        return name;
     }
 }
--- src/main/java/store/service/DataParser.java
+++ src/main/java/store/service/DataParser.java
@@ -1,27 +1,27 @@
 package store.service;
 
+import camp.nextstep.edu.missionutils.DateTimes;
 import store.model.Product;
 import store.model.Promotion;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class DataParser {
     private static final int DATA_HEADER = 1;
     private static final String DELIMITER = ",";
-    private static final String PATTERN_OF_DATE = "yyyy-MM-dd";
     private static final String NO_DATA = "null";
     private static final String EMPTY_PROMOTION = "";
+    private static final int ADDITIONAL_DAY = 1;
 
     public static List<Product> parseProducts(List<String> rawData) {
         return rawData.stream()
                 .skip(DATA_HEADER)
                 .map(data -> {
                     String[] parts = data.split(DELIMITER);
-                    if(parts[3].equals(NO_DATA)){
+                    if (parts[3].equals(NO_DATA)) {
                         return new Product(parts[0], Integer.parseInt(parts[1]), Integer.parseInt(parts[2]), EMPTY_PROMOTION);
                     }
                     return new Product(parts[0], Integer.parseInt(parts[1]), Integer.parseInt(parts[2]), parts[3]);
@@ -34,18 +34,23 @@ public class DataParser {
                 .skip(DATA_HEADER)
                 .map(data -> {
                     String[] parts = data.split(DELIMITER);
-                    return new Promotion(parts[0], Integer.parseInt(parts[1]), Integer.parseInt(parts[2]), parseDate(parts[3]), parseDate(parts[4]));
+                    if(checkPromotionValidity(parts[3], parts[4])) {
+                        return new Promotion(parts[0], Integer.parseInt(parts[1]), Integer.parseInt(parts[2]));
+                    }
+                    return null;
                 })
                 .collect(Collectors.toList());
     }
 
-    private static Date parseDate(String input){
-        SimpleDateFormat formatter = new SimpleDateFormat(PATTERN_OF_DATE);
+    private static boolean checkPromotionValidity(String startDate, String endDate) {
+        LocalDateTime today = DateTimes.now();
+        LocalDate parsedStartDate = LocalDate.parse(startDate);
+        LocalDate parsedEndDate = LocalDate.parse(endDate)
+                .plusDays(ADDITIONAL_DAY);
 
-        try {
-            return formatter.parse(input);
-        } catch (ParseException exception) {
-            return null;
+        if (today.isAfter(parsedStartDate.atStartOfDay()) && today.isBefore(parsedEndDate.atStartOfDay())) {
+            return true;
         }
+        return false;
     }
 }
